aaa1aeb2dfdf99d0d6b74c5d996828289755cf27
Aasi-creoit <aasifa@creoit.com>
2024-02-17 18:23:01 +0530

    sub

--- arithmetic.py
+++ arithmetic.py
@@ -2,3 +2,5 @@ num1=eval(input("ENTER FIRST NUMBER: "));
 num2=eval(input("ENTER SECOND NUMBER: "));
 add=num1+num2;
 print("sum of ",num1"+",num2,"= ",add);
+sub=num1-num2;
+print("sub of ",num1"-",num2,"= ",sub);
